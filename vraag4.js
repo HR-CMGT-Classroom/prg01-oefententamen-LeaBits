23778ea46c8af3327746841a2cb63fa904f4a637
Leanne de Goeij <lr.degoey@gmail.com>
2021-10-26 14:27:35 +0200

    Update vraag4.js

--- vraag4.js
+++ vraag4.js
@@ -1,17 +1,13 @@
 // antwoord op vraag 4
 
 let maxSunblock = 1000
+// let sunBlocks = [pins.A1, pins.A2]
 
+for (let i = 0; i < maxSunblock; i = i + 100) {
+    pins.A1.analogWrite(i)
+    pins.A2.analogWrite(i)
 
-pins.A1.analogWrite(0)
-pins.A2.analogWrite(0)
-pins.A1.analogWrite(100)
-pins.A2.analogWrite(100)
-pins.A1.analogWrite(200)
-pins.A2.analogWrite(200)
-pins.A1.analogWrite(300)
-pins.A2.analogWrite(300)
-pins.A1.analogWrite(400)
-pins.A2.analogWrite(400)
-
-
+    // for(let s = 0; s < sunBlocks.length; s++){
+    //     sunBlocks[s].analogWrite(i)
+    // }
+}
